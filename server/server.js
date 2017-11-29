5fbfe92cc27318f5ca583527b664ab97b5db964f
chowdary001 <jyotheeswar.chowary@habilitertechnologies.com>
2017-11-29 18:44:43 +0530

    project setup and showing chart completed

---
+++ server/server.js
@@ -0,0 +1,9 @@
+var express=require("express");
+var bodyParser=require('body-parser');
+var app = express();
+var path = require('path');
+var http = require('http');
+
+app.use(express.static(path.join(__dirname, '../client')));
+app.listen(8080);
+console.log('Server running on http://localhost:8080');
